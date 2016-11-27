f2b3039b785c654af91c92faa05f6e398453d067
sang-eun <biglove97@hanmail.net>
2016-11-27 17:02:13 +0900

    root time

--- a.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(){
-	char str[80] = "what are we doing";
-	char divide[2][10]={0};
-	sscanf(str,"%s",divide[0]);
-	sscanf(str,"%s",divide[1]);
-	printf("%s %s",divide[0],divide[1]);
-	return 0;
-}
--- root.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-#include "struct_new.h"
-int main(){
-	struct myfs m;
-	m.boot = 
